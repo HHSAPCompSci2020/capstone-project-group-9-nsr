18e547ce7614d296e5475b9fe1c75ea4af5ff79e
nodokashibasaki <nshibasaki217@student.fuhsd.org>
2021-05-06 00:21:17 -0700

    Updated data

--- CapstoneCOVIDVaccineTrackerProject/src/Main.java
+++ CapstoneCOVIDVaccineTrackerProject/src/Main.java
@@ -1,11 +1,14 @@
 /**
 	 * 
 	 */
+import java.io.IOException;
+
 import processing.core.PApplet;
 public class Main {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		State state = new State();
+		state.downloadData();
 		state.readData();
 	}
 
