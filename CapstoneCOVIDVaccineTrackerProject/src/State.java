d5dd94c0ccee744a7340022114aeb6bed18bab99
RoopaS4242 <roopas4242@gmail.com>
2021-05-06 11:13:09 -0700

    added javadoc comments for state

--- CapstoneCOVIDVaccineTrackerProject/src/State.java
+++ CapstoneCOVIDVaccineTrackerProject/src/State.java
@@ -1,17 +1,28 @@
 /*
- * author: 
+ * @author: Roopa Srinivas
+ * date: 05/06/2021
+ * @revision: 1 
  */
 public class State {
 	StatesGraphics graph;
 	
+	/*
+	 * calls the no-args StatesGraphic constructor
+	 */
 	public State() {
 		graph = new StatesGraphics();
 	}
 	
+	/*
+	 * specifies the state for which we are drawing the graphs and obtaining statistics
+	 */
 	public State(String stateName) {
 		graph = new StatesGraphics(stateName);
 	}
 	
+	/*
+	 * draws the graph generated from the StatesGraphics class and writes all the numerical statistics
+	 */
 	public void draw() {
 		
 	}
