b2ab493ebe98fadda3e70456038298ae43dca94c
Sophie Lin <69950657+sophielinn@users.noreply.github.com>
2021-05-06 10:45:25 -0700

    put in fields and methods for CountryMap and Frame

--- CapstoneCOVIDVaccineTrackerProject/src/CountryMap.java
+++ CapstoneCOVIDVaccineTrackerProject/src/CountryMap.java
@@ -1,7 +1,31 @@
-/**
-	 * 
-	 */
+/** @author Sophie
+* date: 5/6/21
+* @revision 1
+*/
+import java.util.ArrayList;
+
+import javax.swing.JComboBox;
+
 import processing.core.PApplet;
 public class CountryMap extends Frame{
-
+	ArrayList<StatesGraphics> states;
+	JComboBox dropDown;
+ 
+	public CountryMap() {
+		states = setAllStates();
+	}
+	private ArrayList<StatesGraphics> setAllStates(){
+		ArrayList<StatesGraphics> setState = new ArrayList<StatesGraphics>();
+	//	setState.add(new State() )
+		for(int i = 0; i < 50; i++) {
+			
+		}
+		return setState;
+	}
+	private void openDropDown() {
+		
+	}
+	private void openStatePage(StatesGraphics state) {
+		
+	}
 }
--- CapstoneCOVIDVaccineTrackerProject/src/Frame.java
+++ CapstoneCOVIDVaccineTrackerProject/src/Frame.java
@@ -8,7 +8,8 @@ import processing.core.PImage;
 public class Frame {
 	private PImage map;
 	private JButton refresh;
-	/**
+	/**refreshes the page so that the information is up to date
+	 * The information refreshes automatically, but this is just in case theres a bug and we need to refresh.
 	 * 
 	 */
 	private void refresh() {
