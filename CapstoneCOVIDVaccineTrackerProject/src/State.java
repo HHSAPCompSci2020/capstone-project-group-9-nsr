dc0530217185a75bc4aadaf46f7913aedefda5a5
RoopaS4242 <roopas4242@gmail.com>
2021-05-23 19:22:58 -0700

    removed flag images and added javadocs to country, state, and moreinfo

--- CapstoneCOVIDVaccineTrackerProject/src/State.java
+++ CapstoneCOVIDVaccineTrackerProject/src/State.java
@@ -31,7 +31,7 @@ public class State extends Frame{
 	private boolean hasBeenReset;
 	
 	/**
-	 * calls the no-args StatesGraphic constructor
+	 * no-args StatesGraphic constructor
 	 */
 	public State() {
 		super("US_MAP.png");
@@ -138,7 +138,6 @@ public class State extends Frame{
 			vaxDistDisplay = vaxDistString;
 		}
 		if (clickDistPercent) {
-			distPercent = vaccine.get(9) + "% of the state population";
 			distPercentDisplay = distPercent;
 		} else {
 			distPercentDisplay = distPercentString;
@@ -154,7 +153,6 @@ public class State extends Frame{
 			vaxedPercentDisplay = vaxedPercentString;
 		}
 		if (clickFullyVaxed) {
-			peopleFullyVaxed = vaccine.get(7);
 			peopleFullyVaxedDisplay = peopleFullyVaxed;
 		} else {
 			peopleFullyVaxedDisplay = peopleFullyVaxedString;
@@ -167,15 +165,9 @@ public class State extends Frame{
 		
 	}
 
-	public void drawButton(PApplet surface, int x, int y, int w, int h, String text, int fillColor) {
-		surface.fill(fillColor);
-		surface.rect(x, y, w, h, 10);
-    	surface.textAlign(surface.CENTER, surface.CENTER);
-    	surface.fill(0);
-    	surface.text(text, x + w/2, y + h/2);
-    	surface.noFill();
-	}
-	
+	/**
+	 * resets all the click boolean fields to false
+	 */
 	public void reset() {
 		
 		setClickDistPercent(false);
@@ -187,6 +179,7 @@ public class State extends Frame{
 		setClickVaxDistributed(false);
 		setHasBeenReset(true);
 	}
+	
 	//get methods
 	/**
 	 * @return name of the state
@@ -195,228 +188,444 @@ public class State extends Frame{
 		return name;
 	}
 	
+	/**
+	 * 
+	 * @return graph associated with this state
+	 */
 	public StatesGraphics getGraph() {
 		return graph;
 	}
 	
+	/**
+	 * 
+	 * @return x coordinate of the button
+	 */
 	public int getButtonX() {
 		return buttonX;
 	}
 	
+	/**
+	 * 
+	 * @return y coordinate of the button
+	 */
 	public int getButtonY() {
 		return buttonY;
 	}
 	
+	/**
+	 * 
+	 * @return width of button
+	 */
 	public int getButtonWidth() {
 		return buttonWidth;
 	}
 	
+	/**
+	 * 
+	 * @return height of button
+	 */
 	public int getButtonHeight() {
 		return buttonHeight;
 	}
 	
+	/**
+	 * 
+	 * @return distance of one button to another
+	 */
 	public float getButtonDistance() {
 		return buttonDistance;
 	}
-		
+	
+	/**
+	 * 
+	 * @return state of clickVaxAvailable field
+	 */
 	public boolean getClickVaxAvailable() {
 		return clickVaxAvailable;
 	}
 	
+	/**
+	 * 
+	 * @return state of clickVaxDistributed field
+	 */
 	public boolean getClickVaxDistributed() {
 		return clickVaxDistributed;
 	}
 	
+	/**
+	 * 
+	 * @return state of clickDistPercent field
+	 */
 	public boolean getClickDistPercent() {
 		return clickDistPercent;
 	}
 	
+	/**
+	 * 
+	 * @return state of clickPeopleVaxed field
+	 */
 	public boolean getClickPeopleVaxed() {
 		return clickPeopleVaxed;
 	}
 	
+	/**
+	 * 
+	 * @return state of clickTotalVaxPercent field
+	 */
 	public boolean getClickTotalVaxPercent() {
 		return clickTotalVaxPercent;
 	}
 	
+	/**
+	 * 
+	 * @return state of clickFullyVaxed field
+	 */
 	public boolean getClickFullyVaxed() {
 		return clickFullyVaxed;
 	}
 	
+	/**
+	 * 
+	 * @return state of clickFullyVaxedPercent field
+	 */
 	public boolean getClickFullyVaxedPercent() {
 		return clickFullyVaxedPercent;
 	}
 	
+	/**
+	 * 
+	 * @return actual statistics for vaxAvailable
+	 */
 	public String getVaxAvailable() {
 		return vaxAvailable;
 	}
 	
+	/**
+	 * 
+	 * @return text that is displayed on the vaxAvailable button
+	 */
 	public String getVaxAvailableDisplay() {
 		return vaxAvailableDisplay;
 	}
 	
+	/**
+	 * 
+	 * @return default text on vaxAvailable button
+	 */
 	public String getVaxAvailableString() {
 		return vaxAvailableString;
 	}
 	
+	/**
+	 * 
+	 * @return actual statistics for vaxDist
+	 */
 	public String getVaxDist() {
 		return vaxDist;
 	}
 	
+	/**
+	 * 
+	 * @return text that is displayed on the vaxDist button
+	 */
 	public String getVaxDistDisplay() {
 		return vaxDistDisplay;
 	}
 	
+	/**
+	 * 
+	 * @return default text on vaxDist button
+	 */
 	public String getVaxDistString() {
 		return vaxDistString;
 	}
 	
+	/**
+	 * 
+	 * @return actual statistics for distPercent
+	 */
 	public String getDistPercent() {
 		return distPercent;
 	}
 	
+	/**
+	 * 
+	 * @return text that is displayed on the distPercent button
+	 */
 	public String getDistPercentDisplay() {
 		return distPercentDisplay;
 	}
 	
+	/**
+	 * 
+	 * @return default text on distPercent button
+	 */
 	public String getDistPercentString() {
 		return distPercentString;
 	}
 	
+	/**
+	 * 
+	 * @return actual statistics for peopleVaxed
+	 */
 	public String getPeopleVaxed() {
 		return peopleVaxed;
 	}
 	
+	/**
+	 * 
+	 * @return text that is displayed on the peopleVaxed button
+	 */
 	public String getPeopleVaxedDisplay() {
 		return peopleVaxedDisplay;
 	}
 	
+	/**
+	 * 
+	 * @return default text on peopleVaxed button
+	 */
 	public String getPeopleVaxedString() {
 		return peopleVaxedString;
 	}
 	
+	/**
+	 * 
+	 * @return actual statistics for vaxedPercent
+	 */
 	public String getVaxedPercent() {
 		return vaxedPercent;
 	}
 	
+	/**
+	 * 
+	 * @return text that is displayed on the vaxedPercent button
+	 */
 	public String getVaxedPercentDisplay() {
 		return vaxedPercentDisplay;
 	}
 	
+	/**
+	 * 
+	 * @return default text on vaxedPercent button
+	 */
 	public String getVaxedPercentString() {
 		return vaxedPercentString;
 	}
 	
+	/**
+	 * 
+	 * @return actual statistics for peopleFullyVaxed
+	 */
 	public String getPeopleFullyVaxed() {
 		return peopleFullyVaxed;
 	}
 	
+	/**
+	 * 
+	 * @return text that is displayed on the peopleFullyVaxed button
+	 */
 	public String getPeopleFullyVaxedDisplay() {
 		return peopleFullyVaxedDisplay;
 	}
 	
+	/**
+	 * 
+	 * @return default text on peopleFullyVaxed button
+	 */
 	public String getPeopleFullyVaxedString() {
 		return peopleFullyVaxedString;
 	}
 	
+	/**
+	 * 
+	 * @return actual statistics for fullyVaxedPercent
+	 */
 	public String getFullyVaxedPercent() {
 		return fullyVaxedPercent;
 	}
 	
+	/**
+	 * 
+	 * @return text that is displayed on the fullyVaxedPercent button
+	 */
 	public String getFullyVaxedPercentDisplay() {
 		return fullyVaxedPercentDisplay;
 	}
 	
+	/**
+	 * 
+	 * @return default text on fullyVaxedPercent button
+	 */
 	public String getFullyVaxedPercentString() {
 		return fullyVaxedPercentString;
 	}
 	
+	/**
+	 * 
+	 * @return state of hasBeenReset field
+	 */
 	public boolean getHasBeenReset() {
 		return hasBeenReset;
 	}
 	
 	//set methods
-	
-	public void setButtonInfo() {
-		
-	}
-	
+
+	/**
+	 * sets buttonX to num
+	 * @param num new x coordinate
+	 */
 	public void setButtonX(int num) {
 		buttonX = num;
 	}
 	
+	/**
+	 * sets buttonY to num
+	 * @param num new y coordinate
+	 */
 	public void setButtonY(int num) {
 		buttonY = num;
 	}
 	
+	/**
+	 * sets buttonWidth to num
+	 * @param num new button width
+	 */
 	public void setButtonWidth(int num) {
 		buttonWidth = num;
 	}
 	
+	/**
+	 * sets buttonHeight to num
+	 * @param num new button height
+	 */
 	public void setButtonHeight(int num) {
 		buttonHeight = num;
 	}
 	
+	/**
+	 * sets buttonDistance to num
+	 * @param num new button distance
+	 */
 	public void setButtonDist(float num) {
 		buttonDistance = num;
 	}
 	
+	/**
+	 * sets clickVaxAvailable to state
+	 * @param state (true or false)
+	 */
 	public void setClickVaxAvailable(boolean state) {
 		clickVaxAvailable = (state);
 	}
 	
+	/**
+	 * sets clickVaxDistributed to state
+	 * @param state (true or false)
+	 */
 	public void setClickVaxDistributed(boolean state) {
 		clickVaxDistributed = (state);
 	}
 	
+	/**
+	 * sets clickDistPercent to state
+	 * @param state (true or false)
+	 */
 	public void setClickDistPercent(boolean state) {
 		clickDistPercent = (state);
 	}
 	
+	/**
+	 * sets clickPeopleVaxed to state
+	 * @param state (true or false)
+	 */
 	public void setClickPeopleVaxed(boolean state) {
 		clickPeopleVaxed = (state);
 	}
 	
+	/**
+	 * sets clickTotalVaxPercent to state
+	 * @param state (true or false)
+	 */
 	public void setClickTotalVaxPercent(boolean state) {
 		clickTotalVaxPercent = (state);
 	}
 	
+	/**
+	 * sets clickFullyVaxed to state
+	 * @param state (true or false)
+	 */
 	public void setClickFullyVaxed(boolean state) {
 		clickFullyVaxed = (state);
 	}
 	
+	/**
+	 * sets clickFullyVaxedPercent to state
+	 * @param state (true or false)
+	 */
 	public void setClickFullyVaxedPercent(boolean state) {
 		clickFullyVaxedPercent = (state);
 	}
 	
+	/**
+	 * sets vaxAvailableDisplay to txt
+	 * @param txt new display text
+	 */
 	public void setVaxAvailableDisplay(String txt) {
 		vaxAvailableDisplay = txt;
 	}
 	
+	/**
+	 * sets vaxDistDisplay to txt
+	 * @param txt new display text
+	 */
 	public void setVaxDistDisplay(String txt) {
 		vaxDistDisplay = txt;
 	}
 	
+	/**
+	 * sets distPercentDisplay to txt
+	 * @param txt new display text
+	 */
 	public void setDistPercentDisplay(String txt) {
-		distPercent = txt;
+		distPercentDisplay = txt;
 	}
 	
+	/**
+	 * sets peopleVaxedDisplay to txt
+	 * @param txt new display text
+	 */
 	public void setPeopleVaxedDisplay(String txt) {
 		peopleVaxedDisplay = txt;
 	}
 	
+	/**
+	 * sets vaxedPercentDisplay to txt
+	 * @param txt new display text
+	 */
 	public void setVaxedPercentDisplay(String txt) {
 		vaxedPercentDisplay = txt;
 	}
 	
+	/** 
+	 * sets peopleFullyVaxedDisplay to txt
+	 * @param txt new display text
+	 */
 	public void setPeopleFullyVaxedDisplay(String txt) {
-		peopleFullyVaxed = txt;
+		peopleFullyVaxedDisplay = txt;
 	}
 	
+	/**
+	 * sets fullyVaxedPercentDisplay to txt
+	 * @param txt new display text
+	 */
 	public void setFullyVaxedPercentDisplay(String txt) {
 		fullyVaxedPercentDisplay = txt;
 	}
 	
+	/**
+	 * sets hasBeenReset to state
+	 * @param state (true or false)
+	 */
 	public void setHasBeenReset(boolean state) {
 		hasBeenReset = state;
 	}
